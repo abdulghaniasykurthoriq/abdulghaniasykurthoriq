93f67bbd61efe029debeb389522734dae8784c4a
abdulghaniasykurthoriq <abdulghaniasykurthoriq@gmail.com>
2023-12-02 12:11:15 +0700

    dropdown navigation fixed

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -9,7 +9,7 @@ function Navbar() {
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-base-100 rounded-box w-52">
         <li><Link href="/">Home</Link></li>
         <li><Link href="/journey">Journey</Link></li>
         
@@ -20,7 +20,7 @@ function Navbar() {
     {/* daisyUI  */}
     </a>
   </div>
-  <div className="navbar-center hidden lg:flex">
+  <div className="navbar-center hidden lg:flex z-50">
     <ul className="menu menu-horizontal px-1">
       <li><Link href="/">Home</Link></li>
       <li><Link href="/journey">Journey</Link></li>
