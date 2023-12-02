8c2f1125e0459caf999cab943e61532db5430464
abdulghaniasykurthoriq <abdulghaniasykurthoriq@gmail.com>
2023-12-02 11:19:58 +0700

    optime quotation mark

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Layout from "./components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <div style={{ height: 'calc(100vh - 70px)' }} className=" flex justify-center items-center ">
+      <div style={{ height: 'calc(100vh - 70px)' }} className="flex justify-center items-center">
         <div className="avatar">
           <div className="w-28 rounded-full ring ring-base-300 ring-offset-base-100 ring-offset-2">
             <Image
@@ -16,15 +16,14 @@ export default function Home() {
           </div>
         </div>
         <div className="flex flex-col md:ml-8 justify-center">
-        <h1 className="text-xl md:text-3xl text-blueGray dark:text-white mt-8 md:mt-0 text-center ml-6 mr-6 md:ml-0 md:mr-0 md:text-start font-regular mb-4">
-  Hello, I&apos;m Agat
-</h1>
-
+          <h1 className="text-xl md:text-3xl text-blueGray dark:text-white mt-8 md:mt-0 text-center ml-6 mr-6 md:ml-0 md:mr-0 md:text-start font-regular mb-4">
+            Hello, I&apos;m Agat
+          </h1>
           <p className="text-base md:text-xl text-blueGray dark:text-white flex font-regular place-self-center md:place-self-start">
-            <span className="text-blue">Software</span>&nbsp;Engineer</p>
-          </div>
+            <span className="text-blue">Software</span>&nbsp;Engineer
+          </p>
+        </div>
       </div>
     </Layout>
-  )
+  );
 }
-
